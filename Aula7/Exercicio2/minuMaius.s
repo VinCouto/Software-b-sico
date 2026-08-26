6075859ca8a6e8e176fd1ca7205db3f9afac4b10
Vinicius Couto <166947709+VinCouto@users.noreply.github.com>
2026-08-26 15:15:37 -0300

    Add files via upload

--- Aula7/Exercicio2/minuMaius.s
+++ Aula7/Exercicio2/minuMaius.s
@@ -1,10 +1,13 @@
-#posições na stack
-    .equ ptrStr, -15
+# posições na stack
+    .equ ptrStr, -16
     .equ localSize, -16
 
+    .section .rodata
 scanFormat:
-    .string "Uma string %i\n"
+    .string "%15[^\n]"
 
+printFormat:
+    .string "%s\n"
 
     .text
     .globl main
@@ -12,18 +15,45 @@ scanFormat:
 main:
     
     pushq %rbp
-    movq %rsp, %rbp
-    addq $localsize, %rsp
+    movq %rsp,%rbp
+    addq $localSize, %rsp
 
 
-    leaq prtStr(%rbp), %rsi #end. da string para rsi
-    movq $scanFormat, %rdi #end. do formato para rdi
-    movq $0, %rax #sem ponto flutuante
+    leaq ptrStr(%rbp), %rsi # end. da string para rsi
+    movq $scanFormat, %rdi # end. do formato para rdi
+    movq $0, %rax # sem ponto flutuante
     call scanf
 
 
+    # movl $56, ptrStr(%rbp)
 
+    pushq %rbx # salva rbx na pilha
+    leaq ptrStr(%rbp), %rbx # rbx aponta para o inicio da pilha
+loop_inicio:
+    movb (%rbx), %al # le um byte
+    cmpb $0, %al # verifica se esta no final da pilha
+    je loop_fim
 
+    cmpb $'a', %al # compara com o primeiro caracter 'a'
+    jb proximo_char # se for menor ja pula para o prox char
+
+    cmpb $'z', %al # compara com o ultimo caracter 'z'
+    ja proximo_char # se for maior pula para o proxima char
+
+    subb $32, %al
+    movb %al, (%rbx)
+
+proximo_char:
+    incq %rbx # incrementa o ponteiro em um byte
+    jmp loop_inicio # volta para o inicio do loop_
+
+loop_fim:
+    popq %rbx # retorna valor original de rbx
+
+    leaq ptrStr(%rbp), %rsi # copia pilha esi
+    movq $printFormat, %rdi   # endereço string em edi
+    movq $0, %rax # sem ponto flutuante
+    call printf # jump e link para printf
 
     movl $0,%eax
     movq %rbp, %rsp
